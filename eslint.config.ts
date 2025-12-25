4d08d119e6c8c08646f039d99177f8570492a1d8
tom <JosephDotson@belkonar.com>
2025-12-24 20:35:43 -0500

    tap

---
+++ eslint.config.ts
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  stylistic.configs.recommended,
+  {
+    rules: {
+      '@stylistic/semi': ['error', 'always'],
+    },
+  },
+]);
