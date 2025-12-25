ada3c37e3899a45d083811ddeaf8e71a95a003a5
tom <JosephDotson@belkonar.com>
2025-12-25 09:54:45 -0500

    tap

--- shared/transport-types.ts
+++ shared/transport-types.ts
@@ -1,3 +1,20 @@
-export interface Hello {
-  name: string;
+// These types are important as they are used for the UI as well.
+
+import z from 'zod';
+
+// May use this, may not. Doesn't really matter.
+export const rpcRequest = z.object({
+  kind: z.string(),
+  body: z.unknown(),
+});
+
+export interface RpcRequest<T> {
+  kind: string;
+  body: T;
 }
+
+export const helloRequest = z.object({
+  name: z.string(),
+});
+
+export type HelloRequest = z.infer<typeof helloRequest>;
